4c06a70eb86966ad665631fd3053d8aec3de30da
Gustavo Passos <gusapas26@gmail.com>
2025-05-15 11:46:07 -0300

    new

--- Routes/PersonRoute.cs
+++ Routes/PersonRoute.cs
@@ -1,3 +1,4 @@
+using PersoTrescon.Data;
 using PersoTrescon.Models;
 
 namespace PersoTrescon.Routes;
@@ -6,9 +7,21 @@ public static class PersonRoute
 {
 public static void PersonRoutes( this WebApplication app)
 {
-    app.MapPost("trescon", () => "");
-    app.MapGet("trescon", () => "");
-    app.MapDelete("trescon", () => "");
+    var route = app.MapGroup("trescon"); // assim nao precisa escrever um por um
+
+
+
+    route.MapPost("trescon", 
+        async (PersonRequest req, PersonContext context) =>
+        {
+            var person = new Person(req.name);
+            await context.AddAsync(person);
+            await context.SaveChangesAsync(); // salva no banco
+            
+        });
+    //app.MapPost("trescon", () => "");
+    //app.MapGet("trescon", () => "");
+    //app.MapDelete("trescon", () => "");
     //app.MapGet("trescon", () => new Person ("Gustavo"));;
 }
 }
